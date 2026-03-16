c3b36d036a8637d8981743c1e1138cf332671f13
falconpwr <falconpwr@gmail.com>
2026-03-16 19:45:06 +0100

    Update pdfRender.js

--- modules/pdfRender.js
+++ modules/pdfRender.js
@@ -1,24 +1,32 @@
-const poppler=require("pdf-poppler")
-const fs=require("fs")
+const { fromPath } = require("pdf2pic")
+const fs = require("fs")
 
-module.exports=async function(pdf){
+module.exports = async function(pdf) {
 
-const opts={
-format:"png",
-out_dir:"uploads",
-out_prefix:"page"
+const convert = fromPath(pdf, {
+density: 150,
+saveFilename: "page",
+savePath: "uploads",
+format: "png",
+width: 1200,
+height: 1600
+})
+
+const pages = []
+
+for(let i=1;i<=20;i++){
+
+try{
+
+const res = await convert(i)
+
+if(res && res.path){
+pages.push(res.path)
 }
 
-await poppler.convert(pdf,opts)
-
-const pages=[]
-
-for(let i=1;i<200;i++){
-
-const file=`uploads/page-${i}.png`
-
-if(fs.existsSync(file))
-pages.push(file)
+}catch(e){
+break
+}
 
 }
 
