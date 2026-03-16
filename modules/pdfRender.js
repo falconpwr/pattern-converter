91f76e4625b163c7c046382152e085e6b5b5da7b
falconpwr <falconpwr@gmail.com>
2026-03-16 20:42:49 +0100

    Update pdfRender.js

--- modules/pdfRender.js
+++ modules/pdfRender.js
@@ -1,16 +1,40 @@
-const sharp = require("sharp")
 const fs = require("fs")
+const path = require("path")
+const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js")
+const { createCanvas } = require("canvas")
 
 module.exports = async function(pdfPath){
 
-const output = "uploads/page-1.png"
+const data = new Uint8Array(fs.readFileSync(pdfPath))
 
-await sharp(pdfPath, { density: 300 })
-.png()
-.toFile(output)
+const pdf = await pdfjsLib.getDocument({data}).promise
 
-console.log("PDF rendered to image:", output)
+const pages = []
 
-return [output]
+for(let i=1;i<=pdf.numPages;i++){
+
+const page = await pdf.getPage(i)
+
+const viewport = page.getViewport({ scale:2 })
+
+const canvas = createCanvas(viewport.width, viewport.height)
+const context = canvas.getContext("2d")
+
+await page.render({
+canvasContext: context,
+viewport
+}).promise
+
+const output = `uploads/page-${i}.png`
+
+fs.writeFileSync(output, canvas.toBuffer("image/png"))
+
+pages.push(output)
+
+}
+
+console.log("PDF rendered pages:", pages)
+
+return pages
 
 }
