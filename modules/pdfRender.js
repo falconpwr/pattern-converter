e4419ec522cc878dd654c82529d97de3ba64e55f
falconpwr <falconpwr@gmail.com>
2026-03-16 21:07:38 +0100

    Update pdfRender.js

--- modules/pdfRender.js
+++ modules/pdfRender.js
@@ -1,47 +1,5 @@
-const fs = require("fs")
-const pdfjs = require("pdfjs-dist")
+module.exports = async function(filePath){
 
-module.exports = async function(pdfPath){
-
-const data = new Uint8Array(fs.readFileSync(pdfPath))
-
-const loadingTask = pdfjs.getDocument({data})
-const pdf = await loadingTask.promise
-
-const pages = []
-
-for(let i=1;i<=pdf.numPages;i++){
-
-const page = await pdf.getPage(i)
-
-const viewport = page.getViewport({scale:2})
-
-const canvasFactory = {
-create: function(width, height) {
-const data = new Uint8ClampedArray(width * height * 4)
-return {canvas:{width,height,data},context:{data}}
-},
-reset(){},
-destroy(){}
-}
-
-const renderContext = {
-canvasContext: canvasFactory.create(viewport.width,viewport.height),
-viewport
-}
-
-await page.render(renderContext).promise
-
-const output = `uploads/page-${i}.raw`
-
-fs.writeFileSync(output, renderContext.canvasContext.data)
-
-pages.push(output)
-
-}
-
-console.log("PDF pages:", pages)
-
-return pages
+return [filePath]
 
 }
