d3ad0b7471399fecdffd47cfd70ff4a8ac60550b
falconpwr <falconpwr@gmail.com>
2026-03-16 20:38:35 +0100

    Update pdfRender.js

--- modules/pdfRender.js
+++ modules/pdfRender.js
@@ -1,47 +1,16 @@
-const fs = require("fs")
-const path = require("path")
 const sharp = require("sharp")
-const pdfjsLib = require("pdfjs-dist")
+const fs = require("fs")
 
 module.exports = async function(pdfPath){
 
-const data = new Uint8Array(fs.readFileSync(pdfPath))
+const output = "uploads/page-1.png"
 
-const pdf = await pdfjsLib.getDocument({data}).promise
-
-  
-const pages = []
-
-for(let i=1;i<=pdf.numPages;i++){
-
-const page = await pdf.getPage(i)
-
-const viewport = page.getViewport({scale:2})
-
-const canvasFactory = {
-create: function(width, height) {
-const data = new Uint8ClampedArray(width * height * 4)
-return { canvas: { width, height, data }, context: { data } }
-},
-reset: function(){},
-destroy: function(){}
-}
-
-const renderContext = {
-canvasContext: canvasFactory.create(viewport.width, viewport.height),
-viewport
-}
-
-await page.render(renderContext).promise
-
-const img = await sharp(Buffer.from(renderContext.canvasContext.data))
+await sharp(pdfPath, { density: 300 })
 .png()
-.toFile(`uploads/page-${i}.png`)
+.toFile(output)
 
-pages.push(`uploads/page-${i}.png`)
+console.log("PDF rendered to image:", output)
 
-}
-console.log("Generated images:", pages)
-return pages
+return [output]
 
 }
